6edcd4418c718df94fa571584bf97cbaaf04e57e
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-12-02 14:32:54 +0500

    End Class 1

--- class-01-hello_world/class1.py
+++ class-01-hello_world/class1.py
@@ -1,2 +1,16 @@
-print("Hello World!")
-print("Pakistan Zindabad!")
+# print("Hello World!")
+# print("Pakistan Zindabad!")
+
+# name = "Nabeel Ahmed"
+# print(name) # print
+# print(type(name)) # type
+# print(id(name)) # physical address of variable
+# # print(dir(name)) # to remove __ from start and end below line of code is used
+# print([i for i in dir(name) if "__" not in i])
+
+
+# name : str = "Nabeel Ahmed"
+# print(name)
+
+name : str = 700
+print(name)
