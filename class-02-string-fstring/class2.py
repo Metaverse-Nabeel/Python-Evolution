cd18a550d70f38140c0d1a65d851465a586105ee
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-12-03 22:21:31 +0500

    Practice string and format string

---
+++ class-02-string-fstring/class2.py
@@ -0,0 +1,8 @@
+# name : str = "Nabeel"
+# print("name") # print the name as it is in the string 
+# print(name) # print the value of variable name
+
+
+message : str = '''My name is Nabeel.
+My age is 29.'''
+print(f"My message is here:\n{message}")
